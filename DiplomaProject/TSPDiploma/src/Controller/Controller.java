dd10883804b7c65e4dc73b66e578021c7fecc286
KKKI <krzysztofiwanczyk5@gmail.com>
2016-11-14 13:13:40 +0000

    init commit

---
+++ DiplomaProject/TSPDiploma/src/Controller/Controller.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import Model.Model;
+import View.MainView;
+
+/**
+ *
+ * @author K
+ */
+public class Controller {
+
+    private MainView view;
+
+    public Controller(Model model) {
+       
+    }
+
+    public void setMainView(MainView mv) {
+      this.view = mv;
+    }
+    
+}
---
+++ DiplomaProject/TSPDiploma/src/tspdiploma/TSPDiploma.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tspdiploma;
+
+import Controller.Controller;
+import Model.Model;
+import View.MainView;
+
+/**
+ *
+ * @author K
+ */
+public class TSPDiploma {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Model model  = new Model();
+        Controller controller = new Controller(model);
+        MainView mainView = new MainView(model, controller);
+        
+        mainView.setVisible(true);
+    }
+    
+}
